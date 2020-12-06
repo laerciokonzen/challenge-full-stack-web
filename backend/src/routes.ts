dbeb979a096577c465fe4c26bc2d791c80d77645
Laercio Konzen <laercio@laerciokonzen.com.br>
2020-12-06 19:11:30 -0300

    Set home route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,2 +1,8 @@
+import { HomeController } from "./controller/HomeController";
 
-export const Routes = [];
+export const Routes = [{
+    method: "get",
+    route: "/",
+    controller: HomeController,
+    action: "index"
+}];
