e77d63eff4bd2a50b1f4f9b0c0060ebc0720ea1e
Laercio Konzen <laercio@laerciokonzen.com.br>
2020-12-06 20:54:57 -0300

    Spec contexts as described on the specified Use Cases

---
+++ backend/tests/integration/Students.spec.ts
@@ -0,0 +1,20 @@
+import { connect } from "net"
+
+describe('Students Endpoints', () => {
+
+    context('Create new Student', () => {
+
+    })
+
+    context('List existing Studends', () => {
+
+    })
+
+    context('Edit Student', () => {
+
+    })
+
+    context('Remove Student', () => {
+
+    })
+})
