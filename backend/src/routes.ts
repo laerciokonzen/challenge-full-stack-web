76be94a80553fe1d88c4682b3851b47ed0cf4abf
Laercio Konzen <laercio@laerciokonzen.com.br>
2020-12-06 22:40:48 -0300

    Register student routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,34 @@
 import { HomeController } from "./controller/HomeController";
+import { StudentController } from "./controller/StudentController";
 
 export const Routes = [{
     method: "get",
     route: "/",
     controller: HomeController,
     action: "index"
+}, {
+    method: "get",
+    route: "/student",
+    controller: StudentController,
+    action: "all"
+}, {
+    method: "get",
+    route: "/student/:academicRegister",
+    controller: StudentController,
+    action: "one"
+}, {
+    method: "post",
+    route: "/student",
+    controller: StudentController,
+    action: "save"
+}, {
+    method: "put",
+    route: "/student",
+    controller: StudentController,
+    action: "edit"
+}, {
+    method: "delete",
+    route: "/student",
+    controller: StudentController,
+    action: "remove"
 }];
