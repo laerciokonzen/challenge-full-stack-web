5b119359490b7b2d2ba8f6733b201f3ef8f327f7
Laercio Konzen <laercio@laerciokonzen.com.br>
2020-12-06 22:40:16 -0300

    Register student routes

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -20,6 +20,7 @@ export default class App {
     routes() {
         Routes.forEach(route => {
             (this.express as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+                console.log(`Registering route for method ${route.method} on ${route.route}`);
                 (new (route.controller as any))[route.action](req, res, next);
             });
         });
